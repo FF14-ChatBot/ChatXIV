89768faa8f9e8e271df26b6b35112069448ca9ff
Alexander Bien <alexanderpbien@gmail.com>
2026-03-04 22:21:48 -0600

    Add frontend scaffolding and frontend CI

---
+++ frontend/eslint.config.js
@@ -0,0 +1,36 @@
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx}"],
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        window: "readonly",
+        document: "readonly",
+      },
+    },
+    settings: {
+      react: { version: "detect" },
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
+  eslintConfigPrettier
+);
---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main>
+      <h1>ChatXIV</h1>
+      <p>Please ask me anything related to FFXIV.</p>
+    </main>
+  );
+}
+
+export default App;
